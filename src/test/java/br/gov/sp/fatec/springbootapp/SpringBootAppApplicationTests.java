7f7d40b3a9abc68b16b6627151d055f3db0548b5
fabsiqueira <fabssiqueira@hotmail.com>
2020-09-15 02:54:30 +0000

    spring data

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -1,13 +1,40 @@
 package br.gov.sp.fatec.springbootapp;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
+
+
 
 @SpringBootTest
-class SpringBootAppApplicationTests {
+//sem nenhuma gravacao usamos transactional e rollback
+@Transactional
+@Rollback
 
-	@Test
+class SpringBootAppApplicationTests {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+    
+    @Test
 	void contextLoads() {
-	}
+    }
+    @Test
+    void testaInsercao(){
+        Usuario usuario = new  Usuario();
+        usuario.setNome("Fabiola");
+        usuario.setSenha("12345");
+        usuarioRepo.save(usuario);
+        assertNotNull(usuario.getId());
+
+
+    }
 
 }
