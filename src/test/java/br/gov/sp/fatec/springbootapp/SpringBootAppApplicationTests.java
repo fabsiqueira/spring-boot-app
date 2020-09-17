5e5a07c6aedc2e249e58e7b3c3c886f0a927707d
fabsiqueira <fabssiqueira@hotmail.com>
2020-09-17 04:17:01 +0000

    spring data jpa 2

--- src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/springbootapp/SpringBootAppApplicationTests.java
@@ -2,20 +2,24 @@ package br.gov.sp.fatec.springbootapp;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
-
+import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
 import br.gov.sp.fatec.springbootapp.entity.Usuario;
+import br.gov.sp.fatec.springbootapp.repository.AutorizacaoRepository;
 import br.gov.sp.fatec.springbootapp.repository.UsuarioRepository;
 
 
 
 @SpringBootTest
 //sem nenhuma gravacao usamos transactional e rollback
-@Transactional
+@Transactional //cada metodo tem uma conexao aberta alem da transacao
 @Rollback
 
 class SpringBootAppApplicationTests {
@@ -23,18 +27,37 @@ class SpringBootAppApplicationTests {
     @Autowired
     private UsuarioRepository usuarioRepo;
     
+    @Autowired
+    private AutorizacaoRepository autRepo; 
+    
     @Test
 	void contextLoads() {
     }
     @Test
     void testaInsercao(){
         Usuario usuario = new  Usuario();
-        usuario.setNome("Fabiola");
-        usuario.setSenha("12345");
+        usuario.setNome("Usuario");
+        usuario.setSenha("senha");
+        usuario.setAutorizacoes(new HashSet<Autorizacao>());
+        Autorizacao aut = new Autorizacao();
+        aut.setNome("ROLE_USUARIO");
+        autRepo.save(aut);
+        usuario.getAutorizacoes().add(aut);
         usuarioRepo.save(usuario);
-        assertNotNull(usuario.getId());
+        assertNotNull(usuario.getAutorizacoes().iterator().next().getId());
+    } 
+    //autorizacoes devem ser previamente salvas no banco ou referencia alguma existente
+    @Test
+    void testaAutorizacao(){
+        Usuario usuario = usuarioRepo.findById(1L).get();
+        assertEquals("ROLE_ADMIN", usuario.getAutorizacoes().iterator().next().getNome());
 
 
     }
-
+    @Test 
+    void testaUsuario(){
+        Autorizacao aut = autRepo.findById(1L).get();
+        assertEquals("Fabiola", aut.getUsuarios().iterator().next().getNome());
+    } 
 }
+
