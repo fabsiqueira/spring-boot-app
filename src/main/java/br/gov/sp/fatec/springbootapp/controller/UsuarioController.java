9c3b721137fe3c9bbb08332f39c4c28e51d806ad
fabssiqueira <fabzsiqueira@gmail.com>
2020-10-14 01:34:10 +0000

    security 2

--- src/main/java/br/gov/sp/fatec/springbootapp/controller/UsuarioController.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,7 +48,8 @@ public class UsuarioController {
     public Usuario buscarPorNome(@RequestParam(value = "nome") String nome){
         return segurancaService.buscarUsuarioPorNome(nome);
     }
-
+    @JsonView(View.UsuarioResumo.class)
+    
     @PostMapping
     public ResponseEntity <Usuario> cadastraNovoUsuario(@RequestBody Usuario usuario, 
         UriComponentsBuilder uriComponentsBuilder){
--- src/main/java/br/gov/sp/fatec/springbootapp/service/SegurancaServiceImpl.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/service/SegurancaServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import br.gov.sp.fatec.springbootapp.entity.Autorizacao;
@@ -42,6 +43,7 @@ public class SegurancaServiceImpl implements SegurancaService {
         return usuario;
     }
     @Override
+    @PreAuthorize("isAuthenticated()") //está autenticado, param entrada
     public List<Usuario> buscarTodosUsuarios(){
         return usuarioRepo.findAll();
         
